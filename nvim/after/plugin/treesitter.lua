49cd2edadab6f7f21d00ce9f880553d2edb651e2
Nebojsa Nadj <nebojsanadj@icloud.com>
2025-09-19 00:07:31 +0200

    feat: UI improvements and dotfile convenience aliases

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "query", "python", "rust", "javascript"},
+  ensure_installed = { "c", "lua", "swift", "query", "python", "rust", "javascript"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
